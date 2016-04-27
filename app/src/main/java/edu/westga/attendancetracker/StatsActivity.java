3579ce37942f1a99b1b6c99f616afb48dc1c05c8
Chris Dunmyer <cdunmye1@my.westga.edu>
2016-04-26 23:49:29 -0400

    Updating the Statistics activity using the MainActivity as a template.

--- app/src/main/java/edu/westga/attendancetracker/StatsActivity.java
+++ app/src/main/java/edu/westga/attendancetracker/StatsActivity.java
@@ -1,29 +1,98 @@
 package edu.westga.attendancetracker;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import edu.westga.attendancetracker.model.Course;
+import edu.westga.attendancetracker.model.Student;
 
 public class StatsActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    private int year, month, day;
+    private Spinner courseSpinner;
+    private ArrayList<Student> arrayOfStudents;
+    private ArrayList<Course> arrayOfCourses;
+    private ListView studentListView;
+    private MyDBHandler dbHandler;
+    private Course currentCourse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        this.dbHandler = new MyDBHandler(this, null, null, 1);
+        this.arrayOfCourses = dbHandler.getCourses();
+        ArrayAdapter<Course> spinnerAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, arrayOfCourses);
+        this.courseSpinner = (Spinner) findViewById(R.id.courseSpinner);
+        this.courseSpinner.setAdapter(spinnerAdapter);
+        // Create student list
+        Course course = (Course) courseSpinner.getSelectedItem();
+        this.arrayOfStudents = dbHandler.getStudentsFromCourse(course.getCourseID());
+        final ArrayAdapter<Student> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, this.arrayOfStudents);
+        final ListView studentListView = (ListView) findViewById(R.id.listView);
+        //studentListView.setScrollbarFadingEnabled(true);
+        studentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(position);
+                StatsActivity.this.arrayOfStudents.get(position).toggleIsPresent();
+                adapter.notifyDataSetChanged();
             }
         });
+        studentListView.setAdapter(adapter);
+        courseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //System.out.println("got here");
+                StatsActivity.this.arrayOfStudents.clear();
+                StatsActivity.this.arrayOfStudents = dbHandler.getStudentsFromCourse(arrayOfCourses.get(position).getCourseID());
+                StatsActivity.this.currentCourse = arrayOfCourses.get(position);
+                for (Student student : StatsActivity.this.arrayOfStudents) {
+                    System.out.println(student.toString());
+                }
+                adapter.clear();
+                adapter.addAll(StatsActivity.this.arrayOfStudents);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        this.arrayOfStudents = dbHandler.getStudentsFromCourse(course.getCourseID());
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+    }
+
+
+    public void didTapEnterAttendance(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
